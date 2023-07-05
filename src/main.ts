073cf345594720daf946c898ad8a86a5eb160bac
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-07-06 00:41:03 +0300

    adding the sessions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import * as session from "express-session";
+import * as passport from "passport";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(
+        session({
+            secret: "my-secret",
+            resave: false,
+            saveUninitialized: false,
+            cookie: { maxAge: 3600000 },
+        }),
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
     await app.listen(3000);
 }
 bootstrap();
