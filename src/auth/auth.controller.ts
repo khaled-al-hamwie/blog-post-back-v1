6058e3c4a991a77b36fb4664abc79a38141d5f94
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-07-06 01:37:17 +0300

    remove the dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,16 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller("auth")
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @Post()
-    register(@Body() createAuthDto: any) {
-        return this.authService.register(createAuthDto);
+    @Post("register")
+    register() {
+        return this.authService.register();
     }
 
-    @Post()
+    @Post("login")
     login() {
         return this.authService.login();
     }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AuthService {
-    register(createAuthDto: any) {
+    register() {
         return "This action adds a new auth";
     }
 
