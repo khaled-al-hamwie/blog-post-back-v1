ae3f546f0aba8cd2b78a751bf0e40b847f3ae109
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-07-06 23:47:32 +0300

    adding the find method

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { FindOneOptions, Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { User } from "./entities/user.entity";
@@ -11,7 +11,12 @@ export class UsersService {
         @InjectRepository(User)
         private usersRepository: Repository<User>,
     ) {}
-    create(createUserDto: CreateUserDto) {
+    async create(createUserDto: CreateUserDto) {
+        const userWithSameUserName: User = await this.findOne({
+            where: { user_name: createUserDto.user_name },
+        });
+        if (userWithSameUserName)
+            throw new ForbiddenException("you can't use this user name");
         const user = this.usersRepository.create(createUserDto);
         this.usersRepository.save(user);
         return user;
@@ -21,8 +26,8 @@ export class UsersService {
         return `This action returns all users`;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} user`;
+    findOne(options: FindOneOptions): Promise<User> {
+        return this.usersRepository.findOne(options);
     }
 
     update(id: number, updateUserDto: UpdateUserDto) {
