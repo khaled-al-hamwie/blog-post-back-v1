eb59135236786b7f508d45c908bffee48d365def
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-01 23:56:30 +0300

    changing the port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from "./modules/app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(MainValidationPipe);
-    await app.listen(3000);
+    await app.listen(4000);
 }
 bootstrap();
