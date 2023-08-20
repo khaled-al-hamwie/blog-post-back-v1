0cc3838e5d50f7cd59b1badc5a24b65b9fa8468d
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-20 20:27:37 +0300

    adding the user ability to the user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RolesModule } from "../roles/roles.module";
 import { User } from "./entities/user.entity";
+import { UsersAbilityFactory } from "./factories/users-ability.factory";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([User]), RolesModule],
     controllers: [UsersController],
-    providers: [UsersService],
+    providers: [UsersService, UsersAbilityFactory],
     exports: [UsersService],
 })
 export class UsersModule {}
