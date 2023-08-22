c41641d0c859747ab0ab544fe9b7c9b3161c1376
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-22 15:46:55 +0300

    adding show profile and get All users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -5,29 +5,46 @@ import {
     Get,
     Param,
     Patch,
-    Post,
+    UnauthorizedException,
+    UseGuards,
 } from "@nestjs/common";
-import { CreateUserDto } from "./dto/create-user.dto";
+import { UserDecorator } from "src/core/common/decorators/user.decorator";
+import { LoggedInGuard } from "src/core/common/guards/logged-in.guard";
+import { Action } from "../auth/enums/actions.enum";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
+import { UsersAbilityFactory } from "./factories/users-ability.factory";
 import { UsersService } from "./users.service";
-
+@UseGuards(LoggedInGuard)
 @Controller("users")
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
-
-    @Post()
-    create(@Body() createUserDto: CreateUserDto) {
-        return this.usersService.create(createUserDto);
-    }
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly usersAbilityFactory: UsersAbilityFactory,
+    ) {}
 
     @Get()
-    findAll() {
-        return this.usersService.findAll();
+    findAll(@UserDecorator() user: User) {
+        const ability = this.usersAbilityFactory.createForUser(user);
+        console.log(user.constructor.name);
+        console.log(ability.can(Action.Read, user));
+        if (ability.can(Action.Read, User))
+            return this.usersService.findAll({});
+        throw new UnauthorizedException();
     }
-
+    @Get("profile")
+    async showProfile(@UserDecorator() user: User) {
+        const ability = this.usersAbilityFactory.createForUser(user);
+        const profile = await this.usersService.findOne({
+            where: { user_id: user.user_id },
+            relations: { role: ability.can(Action.Read, User, "role") },
+        });
+        delete profile.password;
+        return profile;
+    }
     @Get(":id")
-    findOne(@Param("id") id: string) {
-        // return this.usersService.findOne(+id);
+    findOne(@Param("id") user_id: string) {
+        // return this.usersService.findOne(+user_id);
     }
 
     @Patch(":id")
