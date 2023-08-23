0bdd58fa71e98f88f7cb9997af548058e8d1f57e
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-23 13:45:28 +0300

    fixing the user service path

--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
 import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
-import { UsersService } from "src/modules/users/users.service";
+import { UsersService } from "src/modules/users/services/users.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, "local") {
