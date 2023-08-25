99bdf93a38ed95912f1314d911a62ab3d9f1d95f
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-25 16:41:08 +0300

    update log in guard ,throw unauthorized instead of forbidden

--- src/core/common/guards/logged-in.guard.ts
+++ src/core/common/guards/logged-in.guard.ts
@@ -1,8 +1,14 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+} from "@nestjs/common";
 
 @Injectable()
 export class LoggedInGuard implements CanActivate {
     canActivate(context: ExecutionContext) {
-        return context.switchToHttp().getRequest().isAuthenticated();
+        if (context.switchToHttp().getRequest().isAuthenticated()) return true;
+        else throw new UnauthorizedException();
     }
 }
