03b11adb5e361a6f8f5fff5dbb19f74c28726468
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-25 18:44:01 +0300

    like module, importing the users module

--- src/modules/likes/likes.module.ts
+++ src/modules/likes/likes.module.ts
@@ -1,13 +1,15 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BlogsModule } from "../blogs/blogs.module";
+import { UsersModule } from "../users/users.module";
 import { Like } from "./entities/like.entity";
+import { likesAbilityFactory } from "./factories/like-ability.factory";
 import { LikesController } from "./likes.controller";
 import { LikesService } from "./likes.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Like]), BlogsModule],
+    imports: [TypeOrmModule.forFeature([Like]), BlogsModule, UsersModule],
     controllers: [LikesController],
-    providers: [LikesService],
+    providers: [LikesService, likesAbilityFactory],
 })
 export class LikesModule {}
