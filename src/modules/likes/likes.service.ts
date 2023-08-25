3e1a5ebb127494c3e3429d4b8eb4d50674b83110
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-25 18:44:31 +0300

    like service, adding the findOne and create methods

--- src/modules/likes/likes.service.ts
+++ src/modules/likes/likes.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from "@nestjs/common";
-import { CreateLikeDto } from "./dto/create-like.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOneOptions, Repository } from "typeorm";
+import { Blog } from "../blogs/entities/blog.entity";
+import { UsersService } from "../users/services/users.service";
+import { Like } from "./entities/like.entity";
 
 @Injectable()
 export class LikesService {
-    create(createLikeDto: CreateLikeDto) {
-        return "This action adds a new like";
+    constructor(
+        @InjectRepository(Like) private likeRepositry: Repository<Like>,
+        private readonly usersService: UsersService,
+    ) {}
+    async create(blog: Blog, user_id: number) {
+        const like = this.likeRepositry.create();
+        like.blog = blog;
+        like.user = await this.usersService.findOne({
+            where: {
+                user_id,
+            },
+        });
+        this.likeRepositry.save(like);
+        return { message: "like has been added" };
+    }
+
+    findOne(options: FindOneOptions<Like>) {
+        return this.likeRepositry.findOne(options);
     }
 
     remove(id: number) {
