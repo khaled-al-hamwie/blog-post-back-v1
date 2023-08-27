e5600b7c7077bdece0c236af59b49405bd41887b
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-27 18:47:44 +0300

    renaming likes controller to blog likes controller

--- src/modules/likes/controllers/blog-likes.controller.ts
+++ src/modules/likes/controllers/blog-likes.controller.ts
@@ -9,18 +9,18 @@ import {
 } from "@nestjs/common";
 import { UserDecorator } from "src/core/common/decorators/user.decorator";
 import { LoggedInGuard } from "src/core/common/guards/logged-in.guard";
-import { BlogsService } from "../blogs/blogs.service";
-import { BlogAction } from "../blogs/enums/blogs.actions.enum";
-import { BlogNotFoundException } from "../blogs/exceptions/BlogNotFound.exception";
-import { User } from "../users/entities/user.entity";
-import { BlogLike } from "./entities/blog-like.entity";
+import { BlogsService } from "../../blogs/blogs.service";
+import { BlogAction } from "../../blogs/enums/blogs.actions.enum";
+import { BlogNotFoundException } from "../../blogs/exceptions/BlogNotFound.exception";
+import { User } from "../../users/entities/user.entity";
+import { BlogLike } from "../entities/blog-like.entity";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { likesAbilityFactory } from "./factories/like-ability.factory";
-import { LikesService } from "./likes.service";
+import { likesAbilityFactory } from "../factories/like-ability.factory";
+import { LikesService } from "../likes.service";
 
 @UseGuards(LoggedInGuard)
-@Controller("likes")
-export class LikesController {
+@Controller("likes/blogs")
+export class BlogLikesController {
     constructor(
         private readonly likesService: LikesService,
         private readonly blogsService: BlogsService,
@@ -28,7 +28,7 @@ export class LikesController {
     ) {}
 
     @Put(":blog_id")
-    async put(
+    async putLike(
         @Param("blog_id", ParseIntPipe) blog_id: number,
         @UserDecorator() user: User,
     ) {
--- src/modules/likes/likes.module.ts
+++ src/modules/likes/likes.module.ts
@@ -2,10 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BlogsModule } from "../blogs/blogs.module";
 import { UsersModule } from "../users/users.module";
+import { BlogLikesController } from "./controllers/blog-likes.controller";
 import { BlogLike } from "./entities/blog-like.entity";
 import { CommentLike } from "./entities/comment-like.entity";
 import { likesAbilityFactory } from "./factories/like-ability.factory";
-import { LikesController } from "./likes.controller";
 import { LikesService } from "./likes.service";
 
 @Module({
@@ -14,7 +14,7 @@ import { LikesService } from "./likes.service";
         BlogsModule,
         UsersModule,
     ],
-    controllers: [LikesController],
+    controllers: [BlogLikesController],
     providers: [LikesService, likesAbilityFactory],
 })
 export class LikesModule {}
