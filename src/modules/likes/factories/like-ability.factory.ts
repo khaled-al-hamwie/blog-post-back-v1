2598c2b7c0b78eeb3925e87b1dc5f12934482d9a
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-27 19:00:17 +0300

    adding the comment ability

--- src/modules/likes/factories/like-ability.factory.ts
+++ src/modules/likes/factories/like-ability.factory.ts
@@ -6,8 +6,11 @@ import {
 import { Injectable } from "@nestjs/common";
 import { Blog } from "src/modules/blogs/entities/blog.entity";
 import { BlogAction } from "src/modules/blogs/enums/blogs.actions.enum";
+import { Comment } from "src/modules/comments/entities/comment.entity";
+import { CommentAction } from "src/modules/comments/enums/comments.actions.enum";
 import { User } from "src/modules/users/entities/user.entity";
 import { BlogLike } from "../entities/blog-like.entity";
+import { CommentLike } from "../entities/comment-like.entity";
 
 @Injectable()
 export class likesAbilityFactory {
@@ -15,12 +18,16 @@ export class likesAbilityFactory {
         const { can, cannot, build } = new AbilityBuilder(createMongoAbility);
         if (user.role.name == "admin" || user.role.name == "super admin") {
             cannot(BlogAction.LikeBlog, Blog);
+            cannot(CommentAction.LikeComment, Comment);
         } else {
             can(BlogAction.LikeBlog, Blog);
+            can(CommentAction.LikeComment, Comment);
         }
         return build({
             detectSubjectType: item =>
-                item.constructor as ExtractSubjectType<typeof BlogLike>,
+                item.constructor as ExtractSubjectType<
+                    typeof BlogLike | typeof CommentLike
+                >,
         });
     }
 }
