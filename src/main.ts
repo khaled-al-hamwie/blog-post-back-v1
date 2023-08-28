9b92fc755eb27b50c2d51dc772c756aa5b8f886f
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-28 17:39:53 +0300

    adding upload static file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 import { MainValidationPipe } from "./core/common/pipes/main.validation.pipe";
 import { AppModule } from "./modules/app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useGlobalPipes(MainValidationPipe);
+    app.useStaticAssets(join(__dirname, "..", "uploads"));
     await app.listen(4000);
 }
 bootstrap();
