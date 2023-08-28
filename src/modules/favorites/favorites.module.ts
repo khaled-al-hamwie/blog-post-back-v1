da419a7892bc299857c460aff5f3b73954b48ccc
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-28 10:39:04 +0300

    favorite module, importing user and blog service

--- src/modules/favorites/favorites.module.ts
+++ src/modules/favorites/favorites.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { BlogsModule } from "../blogs/blogs.module";
+import { UsersModule } from "../users/users.module";
 import { Favorite } from "./entities/favorite.entity";
 import { FavoritesController } from "./favorites.controller";
 import { FavoritesService } from "./favorites.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Favorite])],
+    imports: [TypeOrmModule.forFeature([Favorite]), BlogsModule, UsersModule],
     controllers: [FavoritesController],
     providers: [FavoritesService],
 })
