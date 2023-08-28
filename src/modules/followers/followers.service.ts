25105175e35136404a82a89a2bf7df0708994e15
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-08-28 15:02:08 +0300

    followers service create, refactor create method

--- src/modules/followers/followers.service.ts
+++ src/modules/followers/followers.service.ts
@@ -18,24 +18,11 @@ export class FollowersService {
         private readonly usersService: UsersService,
     ) {}
     async create(createFollowerDto: CreateFollowerDto) {
-        const alreadyFollow = await this.findOne({
-            where: {
-                user: { user_id: createFollowerDto.user_id },
-                follower: { user_id: createFollowerDto.follower_id },
-            },
-        });
-        if (alreadyFollow) throw new AlreadyFollowedException();
-        const user = await this.usersService.findOne({
-            relations: { role: true },
-            where: { user_id: createFollowerDto.user_id, role: { role_id: 1 } },
-        });
-        if (!user) throw new UserNotFoundException();
-        const follower = await this.usersService.findOne({
-            where: { user_id: createFollowerDto.follower_id },
-        });
+        await this.checkAlreadyFollow(createFollowerDto);
+
         const follow = this.followerEntity.create();
-        follow.follower = follower;
-        follow.user = user;
+        follow.follower = await this.getUser(createFollowerDto.follower_id);
+        follow.user = await this.getUser(createFollowerDto.user_id);
         this.followerEntity.save(follow);
         return { message: "follow has been added" };
     }
@@ -50,5 +37,24 @@ export class FollowersService {
 
     remove(id: number) {
         return `This action removes a #${id} follower`;
+    }
+
+    private async checkAlreadyFollow(createFollowerDto: CreateFollowerDto) {
+        const alreadyFollow = await this.findOne({
+            where: {
+                user: { user_id: createFollowerDto.user_id },
+                follower: { user_id: createFollowerDto.follower_id },
+            },
+        });
+        if (alreadyFollow) throw new AlreadyFollowedException();
+    }
+
+    private async getUser(user_id: number) {
+        const user = await this.usersService.findOne({
+            relations: { role: true },
+            where: { user_id, role: { role_id: 1 } },
+        });
+        if (!user) throw new UserNotFoundException();
+        return user;
     }
 }
